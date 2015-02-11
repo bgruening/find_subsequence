91a4295b9128495a97c4e8c6b75ca804f75c0e96
8 <parastou@ubuntu.ubuntu-domain>
2015-02-11 18:46:38 +0100

    Second retouch

--- Task1.py
+++ Task1.py
@@ -1,45 +1,57 @@
+#!/usr/bin/env python
 
 import argparse
 import logging
 import sys
 from Bio.SeqUtils import nt_search
 from Bio import SeqIO
-import pandas as pd
 
-"""------------------------------ Command-line Parser ----------------------------"""
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('-q', '--query' , required=True)
-parser.add_argument('-p', '--pattern' , required=True)
+choices = ['abi', 'ace', 'clustal', 'embl', 'fasta', 'fastq-sanger', 'fastq', 'fastq-solexa', 'fastq-illumina', 'genbank', 'gb', 'ig' \
+			'imgt', 'nexus', 'phd', 'phylip', 'pir', 'seqxml', 'sff', 'stockholm', 'swiss', 'tab', 'qual', 'uniprot-xml']
 
 
-args = parser.parse_args()
-fq = args.query
-p = args.pattern
-
-
-"""------------------------------ Start the Logger -------------------------------"""
-
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-logger.info('Searching  %s for pattern:%s. Forward strand search...'  %(fq,p))
-
-
-"""------------------------------ Functions---------------------------------------"""
-
-def gf(fasta_sequences):
-	for i in fasta_sequences:
-		sequence, id = i.seq.tostring(), i.id
+def generate_fastas(fasta_sequences):
+	"""
+	Generator for extracting fasta sequences from a Bio.seqIO readable file.
+	Returns the sequence and its ID.
+	"""
+	for f_seq in fasta_sequences:
+		sequence, id = str(f_seq.seq), f_seq.id
 		yield sequence, id
-		
-def find_pattern(query, pattern):
-	l = len(pattern)
-	for (q,id) in query:
-		for match in nt_search(q, pattern):
-			if type(match) == int:
-				sys.stdout.write('{:30s} {:5d}  {:5d}'.format('>'+id,match,match+l)+'\n')
 
-find_pattern(gf(SeqIO.parse(open(fq),'fasta')),p)
+
+def find_pattern(query, pattern):
+	"""
+	Finds all occurrences of a pattern in the query sequence.
+	Outputs sequence ID, start and end postion of the pattern.
+	"""
+	for (q,id) in query:
+		find_match(q,pattern,id)
+		find_match(q[::-1],pattern, id + 'R' )
+
+
+def find_match(query,pattern,id):
+	l = len(pattern)
+	for match in nt_search(query, pattern):
+		if type(match) == int:
+			sys.stdout.write('{:30s} {:5d}  {:5d}'.format('>'+id,match,match+l)+'\n')
+	return
+
+
+if __name__ == "__main__":
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-q', '--query' , required=True)
+	parser.add_argument('-p', '--pattern' , required=True)
+	parser.add_argument('-f', '--format', choices= choices)
+	args = parser.parse_args()
+	query = args.query
+	pattern = args.pattern
+	format = args.format
+	
+	logging.basicConfig(level=logging.INFO)
+	logger = logging.getLogger(__name__)
+	logger.info('Searching  %s for %s. Forward and reverse strand search...'  %(query,pattern))
+	
+	find_pattern(generate_fastas(SeqIO.parse(open(query),format)),pattern)
+	
